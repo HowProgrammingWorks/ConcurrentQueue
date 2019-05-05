e3769b9ca3eaa3ef8b75564b4ed4de99c516c4f6
tshemsedinov <timur.shemsedinov@gmail.com>
2019-05-05 20:47:56 +0300

    Implement priority in queue

---
+++ JavaScript/4-priority.js
@@ -0,0 +1,149 @@
+'use strict';
+
+class Queue {
+  constructor(concurrency) {
+    this.paused = false;
+    this.concurrency = concurrency;
+    this.count = 0;
+    this.waiting = [];
+    this.onProcess = null;
+    this.onDone = null;
+    this.onSuccess = null;
+    this.onFailure = null;
+    this.onDrain = null;
+    this.waitTimeout = Infinity;
+    this.processTimeout = Infinity;
+    this.priorityMode = false;
+  }
+  static channels(concurrency) {
+    return new Queue(concurrency);
+  }
+  wait(msec) {
+    this.waitTimeout = msec;
+    return this;
+  }
+  timeout(msec) {
+    this.processTimeout = msec;
+    return this;
+  }
+  add(task, priority = 0) {
+    if (!this.paused) {
+      const hasChannel = this.count < this.concurrency;
+      if (hasChannel) {
+        this.next(task);
+        return;
+      }
+    }
+    this.waiting.push({ task, start: Date.now(), priority });
+    if (this.priorityMode) {
+      this.waiting.sort((a, b) => b.priority - a.priority);
+    }
+  }
+  next(task) {
+    this.count++;
+    let timer = null;
+    let finished = false;
+    const { processTimeout, onProcess } = this;
+    const finish = (err, res) => {
+      if (finished) return;
+      finished = true;
+      if (timer) clearTimeout(timer);
+      this.count--;
+      this.finish(err, res);
+      if (!this.paused && this.waiting.length > 0) this.takeNext();
+    };
+    if (processTimeout !== Infinity) {
+      const err = new Error('Process timed out');
+      timer = setTimeout(finish, processTimeout, err, task);
+    }
+    onProcess(task, finish);
+  }
+  takeNext() {
+    const { waiting, waitTimeout } = this;
+    const { task, start } = waiting.shift();
+    if (waitTimeout !== Infinity) {
+      const delay = Date.now() - start;
+      if (delay > waitTimeout) {
+        const err = new Error('Waiting timed out');
+        this.finish(err, task);
+        if (waiting.length > 0) {
+          setTimeout(() => {
+            if (this.paused && waiting.length > 0) this.takeNext();
+          }, 0);
+        }
+        return;
+      }
+    }
+    const hasChannel = this.count < this.concurrency;
+    if (hasChannel) this.next(task);
+    return;
+  }
+  finish(err, res) {
+    const { onFailure, onSuccess, onDone, onDrain } = this;
+    if (err) {
+      if (onFailure) onFailure(err, res);
+    } else if (onSuccess) {
+      onSuccess(res);
+    }
+    if (onDone) onDone(err, res);
+    if (this.count === 0 && onDrain) onDrain();
+  }
+  process(listener) {
+    this.onProcess = listener;
+    return this;
+  }
+  done(listener) {
+    this.onDone = listener;
+    return this;
+  }
+  success(listener) {
+    this.onSuccess = listener;
+    return this;
+  }
+  failure(listener) {
+    this.onFailure = listener;
+    return this;
+  }
+  drain(listener) {
+    this.onDrain = listener;
+    return this;
+  }
+  pause() {
+    this.paused = true;
+    return this;
+  }
+  resume() {
+    this.paused = false;
+    if (this.waiting.length > 0) {
+      const hasChannel = this.count < this.concurrency;
+      if (hasChannel) this.takeNext();
+    }
+    return this;
+  }
+  priority(flag = true) {
+    this.priorityMode = flag;
+    return this;
+  }
+}
+
+// Usage
+
+const job = (task, next) => {
+  console.log(`Process: ${task.name}`);
+  setTimeout(next, task.interval, null, task).unref();
+};
+
+const queue = Queue.channels(3)
+  .wait(4000)
+  .timeout(5000)
+  .process(job)
+  .priority()
+  .done((err, task) => console.log(`Done: ${task.name}`))
+  .success(task => console.log(`Success: ${task.name}`))
+  .failure((err, task) => console.log(`Failure: ${err} ${task.name}`))
+  .drain(() => console.log('Queue drain'));
+
+for (let i = 0; i < 10; i++) {
+  const interval = (10 - i) * 1000;
+  queue.add({ name: `Task${i}`, interval }, i);
+}
